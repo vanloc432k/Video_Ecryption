9cf4ceb62b429d5def0fd0356edc332bbbe354de
phn210 <56077387+LYS-SYL@users.noreply.github.com>
2021-07-16 02:06:12 +0700

    update rsa

--- src/camera.py
+++ src/camera.py
@@ -25,11 +25,44 @@ if camera == '0':
 elif camera == '1':
     vid = cv2.VideoCapture('video/beauty.mp4')
 
-# -- Send encryption key -- #
-key = AESGCM.gen()
-key_message = struct.pack(">L", len(key)) + key
-client_socket.sendall(key_message)
+def encrypt_RSA(pk, plaintext):
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on the character using a^b mod m
+    cipher = [pow(ord(char), key, n) for char in plaintext]
+    # Return the array of bytes
+    return cipher
 
+# -- Wait for public key from server for RSA encryption -- #
+data_key = b""
+payload_size = struct.calcsize(">L")
+while len(data_key) < payload_size:
+    packet = client_socket.recv(32)
+    if not packet:
+        break
+    data_key += packet
+packed_msg_size = data_key[:payload_size]
+data_key = data_key[payload_size:]
+msg_size = struct.unpack(">L", packed_msg_size)[0]
+
+while len(data_key) < msg_size:
+    data_key += client_socket.recv(32)
+
+public_key = data_key[:msg_size]
+data_key = data_key[msg_size:]
+public_key = pickle.loads(public_key)
+
+
+# -- Generate encryption key & send to server -- #
+key = AESGCM.gen()
+print(key)
+encrypted_key = encrypt_RSA(public_key, str(key, encoding='latin-1'))
+print(encrypted_key)
+
+serialized_key = pickle.dumps(encrypted_key)
+key_message = struct.pack(">L", len(serialized_key)) + serialized_key
+client_socket.sendall(key_message)
+print('send key done')
 # -- Streaming loop -- #
 while vid.isOpened():
     try:
